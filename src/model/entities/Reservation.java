ae01ccb6e03a79e71dfd3ec07f88696f96757602
JonathanFatecSJC <jonathan.lima.sjc23@gmail.com>
2019-09-07 20:12:48 -0300

    Finalizando Projeto

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
@@ -11,7 +13,10 @@ public class Reservation {
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Data de check-out não pode ser antes de check-in");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -38,20 +43,20 @@ public class Reservation {
 		
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException{
 		
 		Date now = new Date();
 		
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "As datas não podem ser antes da data atual!";
+			throw new DomainException("As datas não podem ser antes da data atual!");
 		}
 		else if (!checkOut.after(checkIn)) {
-			return "Check-out não pode ser antes de check-in!";
+			throw new DomainException("Check-out não pode ser antes de check-in!");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 		
-		return null;
+
 		
 		
 	}
